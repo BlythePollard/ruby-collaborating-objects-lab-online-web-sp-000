33acfe9930ce6cb31a0d6495cc01c835f2d98884
Blythe Pollard <blytheepollard@gmail.com>
2019-10-11 02:27:34 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -16,13 +16,19 @@ class Song
      @@all << self
   end
   
-  def self.new_by_filename(filename)
-    name = filename.split(" - ")[1]
-    artist = filename.split(" - ")[0]
+   def self.new_by_filename(filename)
+    artist, song = filename.split(" - ")
     new_song = self.new(song)
-    new_song.artist.name = artist
+    new_song.artist_name = artist
     new_song
   end
+  # def self.new_by_filename(filename)
+  #   name = filename.split(" - ")[1]
+  #   artist = filename.split(" - ")[0]
+  #   new_song = self.new(song)
+  #   new_song.artist.name = artist
+  #   new_song
+  # end
   
   def artist_name=(name)
     self.artist = Artist.find_or_create_by_name(name)
