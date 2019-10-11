e656babde992681aeb3613ad4045d50e66870499
Blythe Pollard <blytheepollard@gmail.com>
2019-10-11 02:28:26 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -16,12 +16,13 @@ class Song
      @@all << self
   end
 
+ 
   
   def self.new_by_filename(filename)
     name = filename.split(" - ")[1]
     artist = filename.split(" - ")[0]
     new_song = self.new(name)
-    new_song.artist.name = artist
+    new_song.artist_name = artist
     new_song
   end
   
